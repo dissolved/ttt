d3f319c8f34e49d657f03e107f13334c613f15e4
Ryan Sandridge <rsandridge@gmail.com>
2007-12-28 19:30:00 +0000

    Minor tweak.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -30,6 +30,7 @@ class GamesController < ApplicationController
   # PUT /games/1
   def update
     @game = Game.find(params[:id])
+    return unless cookies[:lazy_authentication] == @game.cookie
     
     render :update do |page|
       @game.human_move(params[:move])
@@ -39,7 +40,14 @@ class GamesController < ApplicationController
         page["s#{computer_move}"].replace_html @game[computer_move]
       end
       if @game.finished?
-        page[:flash].replace_html "The only winning move is not to play. #{link_to 'Play Again?', new_game_path}"
+        case @game.board.winner
+        when nil
+          page[:flash].replace_html "The only winning move is not to play. #{link_to 'Play Again?', new_game_path}"
+        when @game.computer
+          page[:flash].replace_html "You lost. #{link_to 'Play Again?', new_game_path}"
+        else
+          page[:flash].replace_html "You're a winner! #{link_to 'Play Again?', new_game_path}"
+        end
       end
     end
   end
